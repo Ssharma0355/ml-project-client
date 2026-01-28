c7cb3e2d5e872516403b0ef571157cc7ffb181cf
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2026-01-28 23:22:31 +0530

    navbar

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div>
-      Navbar
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/login">Login</Link>
     </div>
   )
 }
