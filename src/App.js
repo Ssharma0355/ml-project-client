6ee91f6fc75ab323fee24fe99151847c06b631a8
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2026-01-28 23:20:23 +0530

    saved

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import AppRoutes from './routes/AppRoutes';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+   <BrowserRouter>
+   <AppRoutes />
+   </BrowserRouter>
   );
 }
 
